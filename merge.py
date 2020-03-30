7d1a67358a574defa7750cbc20727ceb7fb5c0d1
Julio-98 <61751781+Julio-98@users.noreply.github.com>
2020-03-30 20:19:39 -0300

    Update merge.py
    first edit in remote repository

--- merge.py
+++ merge.py
@@ -1,4 +1,8 @@
 #Another test!
 #Now a will do some changes here, and in my Vscode
 
-print ("Hello Mr. Julio")
+print ("Hello Mr. Julio") #git pull was done here
+
+#first edit in remote repository
+
+print ("Hello Mr. Julio, I am the Remote Repository")
