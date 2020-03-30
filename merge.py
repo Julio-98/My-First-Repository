52feb6ee4ed60f3522853d52ca092c5b027b7c77
Julio Ramos <ramosjulio771@gmail.com>
2020-03-30 20:31:26 -0300

    first edit in my local repository by Vscode

--- merge.py
+++ merge.py
@@ -2,3 +2,7 @@
 #Now a will do some changes here, and in my Vscode
 
 print ("Hello Mr. Julio")
+
+#first edit in my local repository by Vscode
+
+print ("Hello Mr. Julio, I am your local Repository")
