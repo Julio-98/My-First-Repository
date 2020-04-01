67aebb2e1d35f464c0bff35789d71814e4157741
Julio Ramos <ramosjulio771@gmail.com>
2020-04-01 20:49:50 -0300

    Some recommendations

---
+++ undoing_something.py
@@ -0,0 +1,4 @@
+#Here I studing how can I undo git comands
+#Is important remember that, is not viable use this methods when
+#we are working in a group. It means that we should apply this commands
+#before the git push, in case we are using a git tool.
