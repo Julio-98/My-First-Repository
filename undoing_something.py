b60456d58cdbd5f729590bf8d5b6945226786f36
Julio Ramos <ramosjulio771@gmail.com>
2020-04-02 00:13:04 -0300

    just a test

--- undoing_something.py
+++ undoing_something.py
@@ -2,3 +2,5 @@
 # #Is important remember that, is not viable use this methods when
 # #we are working in a group. It means that we should apply this commands
 # #before the git push, in case we are using a git tool.
+
+print ("good morning world")
