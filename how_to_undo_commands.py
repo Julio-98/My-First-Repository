b59f0ab0b93f109cf6fb62e3ee7fa30b222fbb57
Julio Ramos <ramosjulio771@gmail.com>
2020-04-04 13:17:38 -0300

    Some rocomandtions about this class

---
+++ how_to_undo_commands.py
@@ -0,0 +1,9 @@
+#Here I studing how can I undo git comands
+#Is important remember that, is not viable use this methods when
+#we are working in a group. It means that we should apply this commands
+#before the git push, in case we are using a git tool.
+#Moreover, its a differente class, because I wil do and undo somethings
+#So, when I finish this clas, maybe we will have nothing here
+#But, I register all I have been done in my drive, that we can acsses
+#In my repository.
+#Right, let's go ahead!
