9fc16db68f6ea5b2d59c637492960a4e494d3e71
Julio Ramos <ramosjulio771@gmail.com>
2020-04-06 13:33:40 -0300

    First Commit

---
+++ undoing_somethings.py
@@ -0,0 +1,7 @@
+#Here I studing how can I undo git comands. Is important remember that, is not viable use this methods when
+# #we are working in a group. It means that we should apply this commands before the git push, in case we are using a git tool.
+#Is also important to remember that, here I'll do, and undo somethings... so maybe when I finish this class
+#Will be nothing here, so in order to understand what I do, is important to access the link in my repository
+
+#Let's go ahead!!
+
