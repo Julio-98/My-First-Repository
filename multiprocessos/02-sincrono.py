fd59bdd3d5d1ef8155b55e323e7e016958ede087
Julio <ramosjulio771@gmail.com>
2020-06-15 16:07:03 -0300

    criando e rodando de forma sincrona a funçao que sera utilizada como exemplo

---
+++ multiprocessos/02-sincrono.py
@@ -0,0 +1,23 @@
+import time
+
+
+def any_foo():
+    '''
+        any_foo leva 1 segundo para ser executada
+    '''
+    print('Sleeping 1 segundo...')
+    time.sleep(1)
+    print('Feito sleeping...')
+
+#De maneira síncrona, se eu chamo a função any_foo
+#dua vezes o programa levará 2segundos para ser executado
+
+inicio = time.perf_counter()
+any_foo()
+any_foo()
+fim = time.perf_counter()
+
+print('O tem de execução foi de', fim - inicio,'segundos')
+
+#vejamos quanto tempo leva para a mesma função ser executada 
+#duas vezes quando utilizamos multiprocessing
