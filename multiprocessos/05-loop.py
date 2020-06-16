f02999d0e3f5ccba05609923bb10c38932344079
Julio <ramosjulio771@gmail.com>
2020-06-16 03:22:07 -0300

    rodando com um loop

---
+++ multiprocessos/05-loop.py
@@ -0,0 +1,36 @@
+#Muito bem, para termos uma ideia mais consolidada sobre o assunto
+#criaremos um loop, executanto a função 10 vezes
+
+import multiprocessing
+import time
+
+
+def any_foo():
+    '''
+        any_foo leva 1 segundo para ser executada
+    '''
+    print('Sleeping 1 segundo...')
+    time.sleep(1)
+    print('Feito sleeping...')
+
+inicio = time.perf_counter()
+#Lista de cada processo criada para posteriormente aplicarmos o join()
+processo_list = list()
+#criando um loop para 10 processos
+for _ in range(10):
+#   Por convenção, utilizar o  "_" como variável do for, significa que
+#   está variável não será utilizada dentro do loop. Ex. de uma variável 
+#   utilizada no loop: "for i in range(10): n = n+i"    
+    p = multiprocessing.Process(target=any_foo)
+    p.start()
+    processo_list.append(p)
+
+#aplicando o join(). Neste ponto, deve-se entender que colocar o join
+#dentro do ultimo loop, faria com que nosso código levasse 10 segundos para
+#ser executado. Pois para cada processo, o código ficara parado esperando o retorno
+#da função. Em resumo o código estaria funcionando de maneira síncrona.
+for processo in processo_list:
+    processo.join()
+fim = time.perf_counter()
+
+print('O tempo de execução foi de', fim - inicio,'segundos')
